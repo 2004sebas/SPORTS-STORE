80c215260ff63c8c025f7f5110ece925691b3277
Sebastian Acosta <sebastiamarin2004@gmail.com>
2022-05-05 19:17:39 -0500

    UPDATE: Se actualizaron algunos estilos.SA

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 // Crear productos deportivos
 const products = [
   {id: 1, name: 'CONTROLLER 1', price: "US$ 49.99",img: 'https://m.media-amazon.com/images/I/711wVnVHy0L._AC_SX451_SY423_.jpg/Images/PS4AzulGalaxyStyle.jpg'},
-  {id: 2, name: 'CONTROLLER 2', price: "US$ 40.75",img: 'https://m.media-amazon.com/images/I/61DI81lHiJS._AC_SX451_SY423_.jpg/Images/PS4BlackSkull,jpg'},
+  {id: 2, name: 'CONTROLLER 2', price: "US$ 40.75",img: 'https://m.media-amazon.com/images/I/61DI81lHiJS._AC_SX451_SY423_.jpg/Images/PS4BlackSkull.jpg'},
   {id: 3, name: 'CONTROLLER 3', price: "US$ 39.00",img: 'https://m.media-amazon.com/images/I/61cmN7I7TLL._AC_SX451_SY423_.jpg/Images/PS4OrionNebula.jpg'},
   {id: 4, name: 'CONTROLLER 4', price: "US$ 29.99",img: 'https://m.media-amazon.com/images/I/61El923dV6L._AC_SX451_SY423_.jpg/Images/PS4SakuraPink.jpg'},
   {id: 5, name: 'CONTROLLER 5', price: "US$ 39.75",img: 'https://m.media-amazon.com/images/I/61VnMwi3veL._AC_UY327_FMwebp_QL65_.jpg/Images/PS4RoyalBlue.jpg'},
